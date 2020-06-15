e6034dafd9f165df3758204ba2a8cb87a7478669
Thomas Rider <thomasjrideriii@gmail.com>
2020-06-15 11:54:15 -0400

    Fixed DEFAULT syntax

--- views/db/schema.sql
+++ views/db/schema.sql
@@ -6,18 +6,18 @@ USE pizza_tracker_db;
 
 CREATE TABLE pizzas (
     id INTEGER NOT NULL AUTO_INCREMENT;
-    crust_type VARCHAR(255) DEFAULT ("REGULAR");
-    sause_type VARCHAR(255) DEFAULT ("REGULAR");
-    cheese_type VARCHAR(255) DEFAULT ("REGULAR");
-    topping1 VARCHAR(255) DEFAULT ("NONE");
-    topping2 VARCHAR(255) DEFAULT ("NONE");
-    started_status VARCHAR(255) DEFAULT ("incomplete");
+    crust_type VARCHAR(255) DEFAULT "REGULAR";
+    sause_type VARCHAR(255) DEFAULT "REGULAR";
+    cheese_type VARCHAR(255) DEFAULT "REGULAR";
+    topping1 VARCHAR(255) DEFAULT "NONE";
+    topping2 VARCHAR(255) DEFAULT "NONE";
+    started_status VARCHAR(255) DEFAULT "incomplete";
     started_employee INTEGER;
-    oven_status VARCHAR(255) DEFAULT ("incomplete");
+    oven_status VARCHAR(255) DEFAULT "incomplete";
     oven_employee INTEGER;
-    box_status VARCHAR(255) DEFAULT ("incomplete");
+    box_status VARCHAR(255) DEFAULT "incomplete";
     box_employee INTEGER;
-    delivery_status VARCHAR(255) DEFAULT ("incomplete");
+    delivery_status VARCHAR(255) DEFAULT "incomplete";
     delivery_employee INTEGER;
     PRIMARY KEY (id);
 );

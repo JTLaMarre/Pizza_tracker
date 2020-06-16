7646c6b7178826a44c1cbaff3ce48fea2ba6b4d5
Jesse VanSlyke <jesse.j.vanslyke@gmail.com>
2020-06-16 02:12:34 -0400

    Pushed mysql connection to index.js models, pulled in initial models for index/employee. Also using bcrypt, exrpress-session and passport-local now

---
+++ models/index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else {
+    sequelize = new Sequelize("pizza_tracker_db", "root", "rootroot", {
+        host: "localhost",
+        port: 3306,
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        }
+    })
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(function(file) {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;

e5ba4d5356bf30ad709d695be9d3e0bb40b3f883
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-06-17 13:23:35 -0400

    created seperate js files for each handlebars page and psuedo coded them

---
+++ public/assets/js/newEmployee.js
@@ -0,0 +1,5 @@
+// make sure the employee fields are selected 
+// firstname,lastname
+// make sure the password matches confirmed password if not alert and don't submit
+
+// make sure submit button submits the form data to the data base and creates a new Employee
---
+++ public/assets/js/pizzaManage.js
@@ -0,0 +1,10 @@
+// make sure the dropdown menu for pizzas shows all the pizza ids
+// possibly a handlebars and js working together here, along with the api route
+
+// set the current pizza selected for the page
+
+// display current pizza's id and status
+
+// allow the status dropdown menu to submit a status
+
+// update the current pizza's status in the database and display on this page as well

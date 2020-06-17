b13914c0c71ecb2e42f09caa04c209c8c9741034
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-06-17 10:58:14 -0400

    changed controler to controllers and added the exhbs in the server

--- controllers/html_routes.js
+++ controllers/html_routes.js
@@ -7,7 +7,7 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/buildPizza.html"));
+    res.render("index")
   });
 
   app.get("/tracker", function(req, res) {

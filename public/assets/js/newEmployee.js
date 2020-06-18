13904b269fadd1d4ab8c6d2e92223cb9d151f81f
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-06-18 16:10:13 -0400

    track and employee buttons working

--- public/assets/js/newEmployee.js
+++ public/assets/js/newEmployee.js
@@ -28,8 +28,3 @@ submitEmp.on("click", function (event) {
             })
     }
 });
-// make sure the employee fields are selected
-// firstname,lastname
-// make sure the password matches confirmed password if not alert and don't submit
-
-// make sure submit button submits the form data to the data base and creates a new Employee

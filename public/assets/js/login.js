2e9d12d06de2da722100a32fbb7459d7f95b356e
Jesse VanSlyke <jesse.j.vanslyke@gmail.com>
2020-06-18 12:15:46 -0400

    pushing changes to login- needs work still

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -2,3 +2,35 @@
 
 // the submit buttton submits the form data and then redirects to the pizzaManage page 
 // this should use the passport auth routes in the api routes
+$(document).ready(function() {
+    var loginForm = $("form.login");
+    var empIDInput = $("input#id-input");
+    var passwordInput = $("input#password-input");
+
+    loginForm.on("submit", function(event) {
+        event.preventDefault();
+
+        var employeeData = {
+            employee_id: empIDInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
+
+        if (!employeeData.employee_id || !employeeData.password)
+            return;
+
+        loginEmployee(employeeData.employee_id, employeeData.password);
+        empIDInput.val("");
+        passwordInput.val("");
+    });
+
+    function loginEmployee(employee_id, password) {
+        $.post("/api/login", {
+            employee_id: employee_id,
+            password: password
+        }).then(function() {
+            window.location.replace("/employee/pizza_status");
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+});

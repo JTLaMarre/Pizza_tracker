df06792090671b910e53b13e935bf5e9b3898f91
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-06-18 11:56:00 -0400

    started on adding toppings.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,19 @@
 // make sure drop down options are selecting correctly
+let crust ="";
+let sauce ="";
+let cheese ="";
+let topping1 ="";
+let topping2 ="";
 
+
+$("#submit").click(()=>{
+crust = $("#crust").val()
+sauce = $("#sauce").val()
+cheese = $("#cheese").val()
+topping1 = $("#topping1").val()
+topping2 = $("#topping2").val()
+console.log(crust,sauce,cheese,topping1,topping2)
+})
 // make sure special request is working correctly
 
 // make sure submit button takes the info from the dropdowns and special request

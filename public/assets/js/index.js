9cb04665fc88b4b6336aadda4053ee43dd5bbc73
Christian Gomez Martinez <gomez9289@gmail.com>
2020-06-18 11:22:52 -0400

    index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,20 @@
 // make sure drop down options are selecting correctly
-
+function myFunction(crust) {
+   var x = document.getElementById("crustType").value;
+   document.getElementById("crustType").innerHTML = x;
+   
+    if (crust === "Thin crust") {
+        setCrustType("Thin crust");
+    }else if (crust === "Pan") {
+        setCrustType("Pan");
+    }else if (crust === "Traditional") {
+        setCrustType("Traditional");
+    }else if (crust === "Deep Dish") {
+        setCrustType("Deep dish");
+    }
+  console.log(crust)
+}
+myFunction()
 // make sure special request is working correctly
 
 // make sure submit button takes the info from the dropdowns and special request
